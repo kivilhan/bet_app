a7f4dc3e22bea993fc1e3c95c70e4f667c444430
kivilhan <113761959+kivilhan@users.noreply.github.com>
2025-03-16 11:25:05 +0000

    -

---
+++ betapp/betappApp.swift
@@ -0,0 +1,17 @@
+//
+//  betappApp.swift
+//  betapp
+//
+//  Created by Ilhan on 16/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct betappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
